b06a8001123b867f13e00fb71391a52bad5b6311
Abdullah <ashmaitelly@gmail.com>
2022-04-21 10:26:30 +0300

    Refactored code

--- server/routes/account.js
+++ server/routes/account.js
@@ -8,20 +8,6 @@ const router = express.Router();
 router.get("/signIn", (req, res) => {
     const user = req.body;
     
-    //Old method
-    // UserModel.findOne({username: user.username, password: user.password}, (err, result) => {
-    //   if (err) {
-    //     res.json(err);
-    //   } else {
-    //     if(!result){
-    //       res.sendStatus(404);
-    //     }
-    //     else{
-    //       res.json(result);
-    //       console.log("found");
-    //     } 
-    //   }
-    // });
     UserModel.findOne(user)
     .then(result => {
       if(!result){
@@ -36,30 +22,19 @@ router.get("/signIn", (req, res) => {
   // handle sign-up
   router.post("/signUp", async (req, res) => {
     const user = req.body;
-    
-    // UserModel.findOne({username: user.username}, async (err, result) => {
-    //   if (err) {
-    //     res.json(err);
-    //   } else {
-    //     if(!result){
-    //       const newUser = new UserModel(user);
-    //       await newUser.save();
-    //       res.json(user);
-    //     }
-    //     else{
-    //       res.sendStatus(403);
-    //     } 
-    //   }
-    // });
-    UserModel.findOne(user)
-    .then(result => {
+
+    UserModel.findOne({username : user.username})
+    .then( async result => {
       if(!result){
-        res.status(404).json();
+        const newUser = new UserModel(user);
+        await newUser.save();
+        res.json(user);
       }
       else{
-        res.json(result);
+        res.status(403).json();
       } 
     })
     .catch(err => res.status(500).json({ error: err }));
   });
+
 module.exports = router;
