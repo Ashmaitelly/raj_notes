8cc829de4515e2b5ab514401069c73b2afa04e18
Juliano <juliano.saba2002@gmail.com>
2022-04-21 15:03:31 +0300

    adding a classname

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import SignUp from '../components/SignUp';
+import SignIn from '../components/SignIn';
 
 function Account(){
     return(
+        <div classNAme="account">
         <SignUp />
+        <SignIn />
+        </div>
     );
 }
 
