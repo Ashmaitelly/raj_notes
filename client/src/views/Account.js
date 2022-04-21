6ba3493493a676d3b4f4ca3a8e7a131945e0a204
Juliano <juliano.saba2002@gmail.com>
2022-04-21 16:20:42 +0300

    making buttons

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button } from 'react-bootstrap';
 import SignUp from '../components/SignUp';
 import SignIn from '../components/SignIn';
 
 function Account(){
+    const [haveUser, setHaveUser] = useState(true);
     return(
-        <div classNAme="account">
-        <SignUp />
-        <SignIn />
+        <div className="account">
+        { haveUser ?
+            <SignIn /> :
+            <SignUp />
+        }
+        <Button onClick={()=>{setHaveUser(true)}}>Sign In</Button>
+        <Button onClick={()=>{setHaveUser(false)}}>Sign Up</Button>
+
         </div>
     );
 }
