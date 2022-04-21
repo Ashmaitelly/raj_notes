dcfc18977c446c78d3d6abf4e076974d17d2aa33
Rony <ronyjohn9870@gmail.com>
2022-04-21 14:59:16 +0300

    note section

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 function Note(){
-    
+const TextSection= `text modified by user`;
+    return(
 
+    <Card style={{  height: '80%' , width: '75%', margin: '0 auto' }}>
+  <Card.Body>
+    <Card.Title>Note Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Modified Date</Card.Subtitle>
+    <Card.Text style={{  whiteSpace: "pre-wrap"}}>
+        {TextSection}
+    </Card.Text>
+  </Card.Body>
+</Card>
+    )
 
 }
 
