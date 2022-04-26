85ab05f04a86a0e5980d4e821e8161c2e2e583a2
Juliano <juliano.saba2002@gmail.com>
2022-04-26 11:27:04 +0300

    adding a new file

---
+++ client/src/components/PostComment.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
+
+function PostComments (){
+
+    const [insertComments, setInsertComments] = useState("");
+
+    return(
+        <div>
+        <InputGroup className="mb-2">
+
+                {/* forming the insert comment to the note page */}
+
+
+              <span className="insert comments">
+                <FormControl 
+                  placeholder="insert comments"
+                  aria-label="insert comments"
+                  aria-describedby="insert comments"
+                  value={insertComments}
+                  style={{height: '50%',width: '75%'}}
+                  onChange={(e) => {setInsertComments(e.target.value)}}
+                />
+                </span>
+                <Button style={{height: '10%', width: '5%'}} onClick={()=>{console.log(insertComments)}}>Post</Button>
+                </InputGroup>
+                
+            </div>
+    );
+}
+
+
+export default PostComments;
