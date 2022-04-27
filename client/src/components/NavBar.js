de6be34d8a0a2a2e92bee1ff6eefe85079548a79
Rony <ronyjohn9870@gmail.com>
2022-04-27 14:08:23 +0300

    NavBar Done

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,25 +6,31 @@ function NavBar() {
 
     return(
 
-        <Nav
-             activeKey="/home"
-             onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-        >
-            <Nav.Item>
-                <Nav.Link href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                 <Nav.Link eventKey="Shared-Page">Shared</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="Trash-Page">Deleted</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="Loging-Out" >
-                Log Out
-                </Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">R A J NOTES</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Shared</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Deleted</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav mr-auto">
+            <li class="d-flex justify-content-end nav-item">
+              <a class="nav-link" href="#">Log Out</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
 
 
