4d2687956725055f21ccb2f09636de622680d1cc
Juliano <juliano.saba2002@gmail.com>
2022-04-28 10:24:11 +0300

    styling modification

--- client/src/views/NotesPage.js
+++ client/src/views/NotesPage.js
@@ -7,10 +7,20 @@ export default function NotesPage () {
     return (
         <div>
          <NavBar/>
-         <h2>Your NOTES</h2>
+         <h2 class="text-center">Your NOTES</h2>
          <SearchBar/>
+         <div className="d-flex flex-wrap justify-content-around" style={{width: "80%", marginLeft: "10%"}}>
             <SmallNote color={"#f50"} text={`This is a lovely `}/>
+            <SmallNote color={"#f50"} text={`This is a lovely `}/>
+            <SmallNote color={"#f50"} text={`This is a lovely `}/>
+            <SmallNote color={"#f50"} text={`This is a lovely `}/>
+            <SmallNote color={"#f50"} text={`This is a lovely `}/>
+            <SmallNote color={"#f50"} text={`This is a lovely `}/>
+            <SmallNote color={"#f50"} text={`This is a lovely `}/>
+
+
             <SmallNote color={"#f05"} text={`This is a lovely life if u have a lovely wife where she can take care of u like her own child and gives u all the love and u give her all the protection and security and love all at the same time, life is about sharing and caring, giving and taking.x`}/>
+        </div>
         </div>
     )
 }
