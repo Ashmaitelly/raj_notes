3fc3ef89bad3368f0788b0957efee093b700e0fb
Abdullah <ashmaitelly@gmail.com>
2022-04-29 12:08:32 +0300

    Added additional route to notes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -4,12 +4,23 @@ const NoteModel = require('../models/note');
 
 //get user notes
 router.get("/", (req, res) => {
-    NoteModel.find({})
+    NoteModel.find({soft_deleted: false})
     .then(result => {
         res.json(result);
     })
     .catch(err => res.status(500).json({ error: err }));
   });
+
+//get specific note
+router.get("/:id", (req, res) => {
+  const _id = req.params.id
+  NoteModel.findById(_id)
+  .then(result => {
+      res.json(result);
+  })
+  .catch(err => res.status(500).json({ error: err }));
+});
+
 // new note
 router.post("/Create", async (req, res) => {
   const note = req.body;
