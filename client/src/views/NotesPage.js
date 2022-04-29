f42860c15f4e7412044be4aa2b9fd80b5654e87b
Juliano <juliano.saba2002@gmail.com>
2022-04-29 08:06:47 +0300

    add note page in progress

--- client/src/views/NotesPage.js
+++ client/src/views/NotesPage.js
@@ -1,9 +1,10 @@
 import NavBar from "../components/NavBar"
 import SearchBar from "../components/SearchBar"
 import SmallNote from "../components/SmallNote"
-
+import { useNavigate } from "react-router-dom"
 
 export default function NotesPage () {
+    const navigate= useNavigate();
     return (
         <div>
          <NavBar/>
@@ -22,7 +23,7 @@ export default function NotesPage () {
             <SmallNote color={"#f05"} text={`This is a lovely life if u have a lovely wife where she can take care of u like her own child and gives u all the love and u give her all the protection and security and love all at the same time, life is about sharing and caring, giving and taking.x`}/>
 
             <div>
-            <button style={{position:"fixed", bottom:"25px", right:"25px", cursor:"pointer", width: "55px",height:"55px", padding:"0px 0px" ,borderRadius:"50px", fontSize:"30px", textAlign:"center" }} type="button" class="btn btn-dark">+</button>
+            <button onClick={()=>{navigate("/anp")}} style={{position:"fixed", bottom:"25px", right:"25px", cursor:"pointer", width: "55px",height:"55px", padding:"0px 0px" ,borderRadius:"50px", fontSize:"30px", textAlign:"center" }} type="button" class="btn btn-dark">+</button>
             </div>
          </div>
         </div>
