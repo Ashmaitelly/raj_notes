5e2012ab2e136df45ca58ee65ffc5e878883920e
Rony <ronyjohn9870@gmail.com>
2022-04-29 16:07:25 +0300

    colorSelector is working now

--- client/src/components/ColorSelector.js
+++ client/src/components/ColorSelector.js
@@ -1,19 +1,16 @@
-import Form from 'react-bootstrap/Form'
+import { Dropdown } from 'react-bootstrap'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 function ColorSelector(){
 return(
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Change Color
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Red</a>
-    <a class="dropdown-item" href="#">Black</a>
-    <a class="dropdown-item" href="#">Pink</a>
-    <a class="dropdown-item" href="#">Green</a>
-    <a class="dropdown-item" href="#">Yellow</a>
-  </div>
-</div>
+  <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+    <Dropdown.Item href="#/action-1">Red</Dropdown.Item>
+    <Dropdown.Item href="#/action-2">Yellow</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Green</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Orange</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Light Blue</Dropdown.Item>
+    <Dropdown.Item href="#/action-3">Pink</Dropdown.Item>
+  </DropdownButton>
 );
 }
 export default ColorSelector;
