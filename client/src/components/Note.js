04c559f91af430e90a99665bcf3854f515c3bb77
Juliano <juliano.saba2002@gmail.com>
2022-05-03 15:50:08 +0300

    editing Note

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,23 +1,17 @@
 import React from "react";
-
+import {Card} from 'react-bootstrap'
 
 
 function Note({text}){
 
   return(
-    <div class="card text-center">
-  <div class="card-header">
-    Note Title
-  </div>
-  <div class="input-group">
-  <div class="input-group-prepend">
-  </div>
-  <textarea class="form-control" aria-label="With textarea"></textarea>
-  </div>
-  <div class="card-footer text-muted">
-    modified date
-  </div>
-</div>
+    <Card style={{  height: '80%' , width: '75%', margin: '0 auto' }}>
+    <Card.Body>
+      <Card.Title>Note Title</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">Modified Date</Card.Subtitle>
+      <Card.Text style={{  whiteSpace: "pre-wrap"}}> {text}  </Card.Text>
+    </Card.Body>
+  </Card>
   )
 
 }
