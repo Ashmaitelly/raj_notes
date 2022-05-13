8f5ce7cb4e2509f70f01abfce2fe589823796f99
Abdullah <ashmaitelly@gmail.com>
2022-05-13 09:55:59 +0300

    Small update to shared

--- server/routes/shared.js
+++ server/routes/shared.js
@@ -1,32 +1,29 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const NoteModel = require('../models/note');
+const NoteModel = require("../models/note");
 
 //get soft deleted user notes
 router.get("/", (req, res) => {
-    NoteModel.find({shared: true})
-    .then(result => {
-        res.json(result);
+  NoteModel.find({ shared: true, shared_users: "b" })
+    .select("title date_modified text bgc")
+    .then((result) => {
+      res.json(result);
     })
-    .catch(err => res.status(500).json({ error: err }));
-  });
+    .catch((err) => res.status(500).json({ error: err }));
+});
 
 //get specific note
 router.get("/:id", (req, res) => {
-  const _id = req.params.id
-  NoteModel.findOne({_id: _id, shared: true})
-  .then(result => {
-    if(!result){
-      res.status(404).json();
-    }
-    else{
-      res.json(result);
-    }
-  })
-  .catch(err => res.status(500).json({ error: err }));
+  const _id = req.params.id;
+  NoteModel.findOne({ _id: _id, shared: true })
+    .then((result) => {
+      if (!result) {
+        res.status(404).json();
+      } else {
+        res.json(result);
+      }
+    })
+    .catch((err) => res.status(500).json({ error: err }));
 });
-
-
-
 
 module.exports = router;
