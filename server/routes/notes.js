84e6a566cefb94838e7ebf2cbfe2425e5948692d
Juliano <juliano.saba2002@gmail.com>
2022-05-18 08:31:53 +0300

    adding the share with button to functionality

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -60,6 +60,24 @@ router.put("/update/:id", (req, res) => {
     })
     .catch((err) => res.status(500).json({ error: err }));
 });
+// share with button activation
+router.put("/update/:id", (req, res) => {
+  const _id = req.params.id;
+  const note = req.body;
+  const update = {
+    shared: true,
+    $push: {shared_users:[note.user]}
+  };
+  NoteModel.findOneAndUpdate({ _id: _id }, update)
+    .then((result) => {
+      if (!result) {
+        res.status(404).json();
+      } else {
+        res.json(result);
+      }
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
 //change color code here
 
 module.exports = router;
