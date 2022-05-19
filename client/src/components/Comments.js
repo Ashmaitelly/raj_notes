7bb1afddb7b11c8e3e62e160b86c693d1b6fbc87
Abdullah <ashmaitelly@gmail.com>
2022-05-19 08:46:16 +0300

    Setting up comments

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,15 +1,18 @@
-import React from "react";
-import {Stack} from 'react-bootstrap';
+import React, { useContext } from "react";
+import { Stack, Card } from "react-bootstrap";
+import { CommentsContext } from "../App";
 
-function Comments(){
-
-    return (
-
+function Comments() {
+  const comments = useContext(CommentsContext);
+  return (
     <Stack gap={3}>
-       <div className="bg-light border">Comment</div>
+      <div className="bg-light border">Comment</div>
+      <Card style={{ padding: "10px 0px" }}>
+        <Card.Subtitle>{`User - Date`}</Card.Subtitle>
+        <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
     </Stack>
-
-    )
+  );
 }
 
-export default Comments ;
+export default Comments;
