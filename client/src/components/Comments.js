3cdb87c87e3eeaf4b5b3235f9e0782ff66fe67f7
Abdullah <ashmaitelly@gmail.com>
2022-05-19 08:58:02 +0300

    Comments display now

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { Stack, Card } from "react-bootstrap";
 import { CommentsContext } from "../App";
+import Moment from "moment";
 
 function Comments() {
   const comments = useContext(CommentsContext);
   return (
     <Stack gap={3}>
       <div className="bg-light border">Comment</div>
-      <Card style={{ padding: "10px 0px" }}>
-        <Card.Subtitle>{`User - Date`}</Card.Subtitle>
-        <Card.Body>This is some text within a card body.</Card.Body>
-      </Card>
+      {comments.map((comment, index) => (
+        <Card style={{ padding: "10px 0px" }}>
+          <Card.Subtitle>{`${comment.username} - ${Moment(comment.time).format(
+            "MMMM Do YYYY, h:mm:ss a"
+          )}`}</Card.Subtitle>
+          <Card.Body>{`${comment.comment}`}</Card.Body>
+        </Card>
+      ))}
     </Stack>
   );
 }
