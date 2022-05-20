0e6aac7781894320579c3fd4279f489838a1934c
Abdullah <ashmaitelly@gmail.com>
2022-05-20 08:48:14 +0300

    Added delete button

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Stack, Card } from "react-bootstrap";
+import { Stack, Card, Button } from "react-bootstrap";
 import { CommentsContext } from "../App";
 import Moment from "moment";
 
@@ -8,14 +8,21 @@ function Comments() {
   return (
     <Stack gap={3}>
       <div className="bg-light border">Comment</div>
-      {comments.map((comment, index) => (
-        <Card key={index} style={{ padding: "10px 0px" }}>
-          <Card.Subtitle>{`${comment.username} - ${Moment(comment.time).format(
-            "MMMM Do YYYY, h:mm:ss a"
-          )}`}</Card.Subtitle>
-          <Card.Body>{`${comment.comment}`}</Card.Body>
-        </Card>
-      )).reverse()}
+      {comments
+        .map((comment, index) => (
+          <Card key={index} style={{ padding: "10px 0px" }}>
+            <Card.Subtitle>{`${comment.username} - ${Moment(
+              comment.time
+            ).format("MMMM Do YYYY, h:mm:ss a")}`}</Card.Subtitle>
+            <Card.Body>
+              {`${comment.comment}`}
+              <Button variant="danger" style={{ float: "right" }}>
+                X
+              </Button>
+            </Card.Body>
+          </Card>
+        ))
+        .reverse()}
     </Stack>
   );
 }
