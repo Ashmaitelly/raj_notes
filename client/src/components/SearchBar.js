061dd3d26a109e948c1eac77160d5ba419ef1575
Rony <ronyjohn9870@gmail.com>
2022-05-26 15:40:00 +0300

    SearchBar New Layout and CSS

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -5,17 +5,12 @@ import { SearchContext } from "../App";
 function SearchBar() {
   const setSearch = useContext(SearchContext);
   return (
-    <div>
-      <InputGroup className="mb-3">
-        <FormControl
-          placeholder="Search Here"
-          aria-label="Search Here"
-          aria-describedby="basic-addon2"
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-      </InputGroup>
+    <div style={{ marginBottom:"15px"}}>
+      <input type="text" name="search" placeholder="Search By Title"
+
+onChange={(e) => {
+      setSearch(e.target.value);
+    }}></input>
     </div>
   );
 }
