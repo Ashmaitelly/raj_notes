c228d33886ce0119ecf6957e0cc6c43417178b81
Juliano <juliano.saba2002@gmail.com>
2022-05-26 15:49:33 +0300

    fixed navbar bug

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import { useNavigate } from "react-router-dom";
 
