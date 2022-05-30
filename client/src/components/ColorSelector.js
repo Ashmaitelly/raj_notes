e4c6d41fb28bd88d41a0f9e9d735827a2519ced6
Abdullah <ashmaitelly@gmail.com>
2022-05-30 09:01:18 +0300

    Fixed Names

--- client/src/components/ColorSelector.js
+++ client/src/components/ColorSelector.js
@@ -5,13 +5,13 @@ import { ColorContext } from "../App";
 
 function ColorSelector() {
   const [cols] = useState([
-    "red",
-    "yellow",
-    "green",
-    "orange",
-    "light blue",
-    "pink",
-    "white",
+    "Red",
+    "Yellow",
+    "Green",
+    "Orange",
+    "Aqua",
+    "Pink",
+    "White",
   ]);
 
   const setCol = useContext(ColorContext);
@@ -29,69 +29,6 @@ function ColorSelector() {
           {col}
         </Dropdown.Item>
       ))}
-      {/* <Dropdown.Item
-        onClick={() => {
-          setCol("#F6041C");
-        }}
-        type="color"
-        defaultValue="#F6041C"
-      >
-        Red
-      </Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          setCol("#F6D604");
-        }}
-        type="color"
-        defaultValue="#F6D604"
-      >
-        Yellow
-      </Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          setCol("#24AB12");
-        }}
-        type="color"
-        defaultValue="#24AB12"
-      >
-        Green
-      </Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          setCol("#F60");
-        }}
-        type="color"
-        defaultValue="#F60"
-      >
-        Orange
-      </Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          setCol("#0EF");
-        }}
-        type="color"
-        defaultValue="#0EF"
-      >
-        Light Blue
-      </Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          setCol("#FF00EA");
-        }}
-        type="color"
-        defaultValue="#FF00EA"
-      >
-        Pink
-      </Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          setCol("#fff");
-        }}
-        type="color"
-        defaultValue="#FFF"
-      >
-        White
-      </Dropdown.Item> */}
     </DropdownButton>
   );
 }
