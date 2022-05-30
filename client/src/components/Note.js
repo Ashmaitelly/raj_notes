d1b55d9f3fcfc6b0154039acf8b50a0d726bc931
Abdullah <ashmaitelly@gmail.com>
2022-05-30 11:27:04 +0300

    Fixed bug with author displaying

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -5,16 +5,22 @@ import Moment from "moment";
 
 function Note() {
   const note = useContext(NotesContext);
-  const [user] = localStorage.getItem("user");
 
   return (
     <div>
-      <Card className="mx-auto mb-3" style={{ marginTop: "20px", minHeight: "450px", width: "75%" }}>
+      <Card
+        className="mx-auto mb-3"
+        style={{ marginTop: "20px", minHeight: "450px", width: "75%" }}
+      >
         <Card.Body style={{ backgroundColor: note.bgc }}>
-          <Card.Title>
-            {note.title}{" "}
-            {note.author === user ? "" : <small>{`~by ${note.author}`}</small>}
-          </Card.Title>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            {note.author === localStorage.getItem("user") ? (
+              ""
+            ) : (
+              <small>{`by ${note.author}`}</small>
+            )}
+          </Card.Subtitle>
           <Card.Subtitle className="mb-2 text-muted">
             Last Modified:{" "}
             {Moment(note.date_modified).format("MMMM Do YYYY, h:mm:ss a")}
