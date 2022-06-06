6ca1a29ad496588d04a62796ae78ae776fe83876
Juliano <juliano.saba2002@gmail.com>
2022-06-06 08:39:00 +0300

    making downloaded bcrypt and worked in server

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt= require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -10,6 +11,19 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+//hashing the password in the database
+UserSchema.pre('save', async function(next){
+  try {
+    const salt=await bcrypt.genSalt(10);
+    const hashedPassword= await bcrypt.hash(this.password,salt)
+    this.password= hashedPassword;
+    next();
+  } catch (error) {
+    next(err);
+  }
+});
+
+
 
 const UserModel = mongoose.model("user", UserSchema);
 module.exports = UserModel;
