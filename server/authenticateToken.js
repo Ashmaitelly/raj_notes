19a0151ace928a757e68f092671b734051e7956a
AbdSh <ashmaitelly@gmail.com>
2022-06-14 11:55:01 +0300

    Trying

--- server/authenticateToken.js
+++ server/authenticateToken.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI);
 
+const TokenModel = require('./models/token');
+
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const authArray = authHeader && authHeader.split(' ');
@@ -12,15 +14,38 @@ function authenticateToken(req, res, next) {
   if (accessToken == null) return res.sendStatus(401);
 
   jwt.verify(accessToken, process.env.ACCESS_TOKEN, (err, user) => {
+    let auth = {};
     if (user) {
-      req.user = user;
-      next();
+      auth = { user: user };
     }
+    if (err) {
+      try {
+        const refresh = refreshToken(refreshToken);
+        auth = { user: refresh[0], token: refresh[1] };
+      } catch (error) {
+        res.sendStatus(403).json({ message: 'Access denied' });
+      }
+    }
+    req.auth = auth;
+    next();
   });
 }
 
-function refreshToken(refreshToken, res) {
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {});
+function refreshToken(refreshToken) {
+  const refresh = [];
+  TokenModel.find({ token: refreshToken }).then((result) => {
+    if (result) {
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user) => {
+        if (err) throw new Error('Invalid refresh token');
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+          expiresIn: '30m',
+        });
+        refresh.push(user, token);
+      });
+    }
+  });
+
+  return refresh;
 }
 
 module.exports = authenticateToken;
